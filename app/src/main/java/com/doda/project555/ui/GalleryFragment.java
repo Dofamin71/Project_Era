b722a3982e2d2a4fa84ea3c527e1fd090f96780b
Dareten <daretens@gmail.com>
2020-04-01 17:27:42 +0300

    Initial D

---
+++ app/src/main/java/com/doda/project555/ui/GalleryFragment.java
@@ -0,0 +1,25 @@
+package com.doda.project555.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.doda.project555.R;
+
+public class GalleryFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        final TextView textView = root.findViewById(R.id.text_gallery);
+        textView.setText(R.string.gallery);
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/doda/project555/ui/SlideshowFragment.java
@@ -0,0 +1,24 @@
+package com.doda.project555.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.doda.project555.R;
+
+public class SlideshowFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        final TextView textView = root.findViewById(R.id.text_slideshow);
+        textView.setText(R.string.slideshow);
+
+        return root;
+    }
+}
