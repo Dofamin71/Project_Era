e662ce087463248c6b8f5e6cc0ae90030441682b
Dareten <daretens@gmail.com>
2020-04-03 01:23:11 +0300

    Что-то сделалось

--- app/src/main/java/com/doda/project555/MainActivity.java
+++ app/src/main/java/com/doda/project555/MainActivity.java
@@ -46,7 +46,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "НУ И ЧЕГО ТЫ ТЫКАЕШЬ, РОБИТ ОНО",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),
+                        "НУ И ЧЕГО ТЫ ТЫКАЕШЬ, РОБИТ ОНО",Toast.LENGTH_SHORT).show();
                 openCalc(view);
             }
         });
