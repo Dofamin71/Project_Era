7f0810cb4b2fe393a108bb78ee77813b0a6073d8
Dareten <daretens@gmail.com>
2020-04-13 01:39:40 +0300

    Тут код почищен и null убран

--- app/src/main/java/com/doda/project555/CalcActivity.java
+++ app/src/main/java/com/doda/project555/CalcActivity.java
@@ -1,14 +1,12 @@
 package com.doda.project555;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.button.MaterialButton;
 
--- app/src/main/java/com/doda/project555/ui/HomeFragment.java
+++ app/src/main/java/com/doda/project555/ui/HomeFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,9 +15,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-
-
-        return root;
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
